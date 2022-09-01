6d4946c59a1b2b785f820db8e43aae0c202f44e2
Antti Pynnönen <anttipyn@student.uef.fi>
2022-09-01 20:24:09 +0300

    MusicPlayer prop scSrc changed in Aani component

--- src/components/Aani/Aani.tsx
+++ src/components/Aani/Aani.tsx
@@ -6,7 +6,7 @@ interface AaniProps {}
 
 const Aani: FC<AaniProps> = () => (
   <div className={styles.Aani}>
-  <MusicPlayer scSrc='https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/tracks/3887664' imageSrc={`${process.env.PUBLIC_URL}/images/aurinko.png`} />
+  <MusicPlayer scSrc='https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/tracks/236815210' imageSrc={`${process.env.PUBLIC_URL}/images/aurinko.png`} />
   </div>
 );
 
